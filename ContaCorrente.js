e677cb3f4de45e43b2966226160ee1d329bac12d
Julia Magalhães <juliamagalhaes28@outlook.com>
2021-01-14 00:18:26 -0300

    definição de null e undefined

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -20,5 +20,7 @@ export class ContaCorrente{
     transferir(valor, conta){
         const valorSacado = this.sacar(valor);
         conta.depositar(valorSacado);
+        conta.cidade = "Piraí";
+        valor = 20;
     }
 }
--- index.js
+++ index.js
@@ -13,26 +13,30 @@ cliente1.nome = "Julia";
 cliente1.cpf = "11111111111";
 cliente1.rg = '123456789'
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = "00000000000";
-cliente2.rg = '987654321';
+// const cliente2 = new Cliente();
+// cliente2.nome = "Alice";
+// cliente2.cpf = "00000000000";
+// cliente2.rg = '987654321';
+// ctrl + ;
 
 const contaCorrenteJulia = new ContaCorrente();
 contaCorrenteJulia.agencia = 1001;
 contaCorrenteJulia.cliente = cliente1;
-
 contaCorrenteJulia.depositar(200);
 const valorSacado = contaCorrenteJulia.sacar(50);
 
 const contaCorrente2 = new ContaCorrente();
+contaCorrente2.cliente = new Cliente();
+contaCorrente2.cliente.nome = "Nathan";
+contaCorrente2.cliente.cpf = "00000000000";
 contaCorrente2.agencia = 1001;
-contaCorrente2.cliente = cliente2;
 
-contaCorrenteJulia.transferir(10, contaCorrente2);
+let valor = 200;
+contaCorrenteJulia.transferir(valor, contaCorrente2);
 
-console.log(valorSacado)
-console.log(contaCorrenteJulia);
+// console.log("valor: ", valor);
+// console.log(valorSacado)
+// console.log(contaCorrenteJulia);
 console.log(contaCorrente2);
 
 //console.log(cliente1);
